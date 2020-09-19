3fb6209f08088a64aff69504fd7afbbc743a98f7
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-19 01:01:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,10 +13,14 @@ emoticons = YAML.load_file(file)
  final_hash
 end
 
+
+
 def get_japanese_emoticon(file, emoticon)
   load_library(file).each {|key, value| if value[:english] == emoticon then  return value[:japanese] end}
   return "Sorry, that emoticon was not found"
 end
+
+
 
 def get_english_meaning(file, emoticon)
   load_library(file).each do |key, value|
